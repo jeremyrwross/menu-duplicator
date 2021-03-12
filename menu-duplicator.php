9748c7891336e4f548035e1d8508e2d05f3d6447
Jeremy Ross <jeremyrwross@gmail.com>
2021-03-12 09:14:15 -0400

    bump version and tested numbers

--- menu-duplicator.php
+++ menu-duplicator.php
@@ -3,11 +3,11 @@
  * Plugin Name: Menu Duplicator
  * Plugin URI: http://jereross.com/menu-duplicator/
  * Description: Quickly duplicate WordPress menus
- * Version: 0.5
+ * Version: 0.6
  * Author: Jeremy Ross
  * Author URI: http://jereross.com
  * Requires at least: 4.0
- * Tested up to: 5.5
+ * Tested up to: 5.7
  *
  * Menu Duplicator is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,18 +26,18 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (! defined('ABSPATH')) {
+    exit;
 }
 
 // Only execute inside the dashboard.
-if ( ! is_admin() ) {
-	return;
+if (! is_admin()) {
+    return;
 }
 
-define( 'MD_VERSION',		'0.5' );
-define( 'MD_TOOLS_PAGE',	esc_url( admin_url( 'tools.php' ) ).'?page=menu-duplicator' );
-define( 'MD_PLUGIN_URL',	plugin_dir_url( __FILE__ ) );
+define('MD_VERSION', '0.5');
+define('MD_TOOLS_PAGE', esc_url(admin_url('tools.php')).'?page=menu-duplicator');
+define('MD_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 
 /**
@@ -47,12 +47,11 @@ define( 'MD_PLUGIN_URL',	plugin_dir_url( __FILE__ ) );
  *
  * @since Menu Duplicator 0.1
  */
-function menu_duplicator_create_page() {
-
-	add_management_page( 'Menu Duplicator', 'Menu Duplicator', 'edit_theme_options', 'menu-duplicator', 'menu_duplicator_settings_page' );
-
+function menu_duplicator_create_page()
+{
+    add_management_page('Menu Duplicator', 'Menu Duplicator', 'edit_theme_options', 'menu-duplicator', 'menu_duplicator_settings_page');
 }
-add_action( 'admin_menu', 'menu_duplicator_create_page' );
+add_action('admin_menu', 'menu_duplicator_create_page');
 
 
 /**
@@ -62,31 +61,30 @@ add_action( 'admin_menu', 'menu_duplicator_create_page' );
  *
  * @since Menu Duplicator 0.1
  */
-function menu_duplicator_process_form() {
+function menu_duplicator_process_form()
+{
 
-	// Allow only users with edit_theme_options capabilities.
-	if ( ! current_user_can( 'edit_theme_options' ) ) {
-		return;
-	}
+    // Allow only users with edit_theme_options capabilities.
+    if (! current_user_can('edit_theme_options')) {
+        return;
+    }
 
-	// Check for POST and wpnonce.
-	if ( isset( $_POST['type'] ) && 'menu-duplicator' === $_POST['type'] && check_admin_referer( 'menu-duplicator', 'menu-duplicator-nonce' ) ) {
+    // Check for POST and wpnonce.
+    if (isset($_POST['type']) && 'menu-duplicator' === $_POST['type'] && check_admin_referer('menu-duplicator', 'menu-duplicator-nonce')) {
+        if (! isset($_POST['menu-to-duplicate'])) { // Input var okay.
+            menu_duplicator_admin_message('error', 'Menu to duplicate was not supplied. Please select a menu from the list below.');
+            return;
+        }
 
-		if ( ! isset( $_POST['menu-to-duplicate'] ) ) { // Input var okay.
-			menu_duplicator_admin_message( 'error', 'Menu to duplicate was not supplied. Please select a menu from the list below.' );
-			return;
-		}
+        if (! isset($_POST['new-menu-name'])) { // Input var okay.
+            menu_duplicator_admin_message('error', 'Name for the new menu was not supplied. Please input a name for the menu below.');
+            return;
+        }
 
-		if ( ! isset( $_POST['new-menu-name'] ) ) { // Input var okay.
-			menu_duplicator_admin_message( 'error', 'Name for the new menu was not supplied. Please input a name for the menu below.' );
-			return;
-		}
-
-		menu_duplicator_settings_update( $_POST['menu-to-duplicate'], $_POST['new-menu-name'] );
-
-	}
+        menu_duplicator_settings_update($_POST['menu-to-duplicate'], $_POST['new-menu-name']);
+    }
 }
-add_action( 'init', 'menu_duplicator_process_form' );
+add_action('init', 'menu_duplicator_process_form');
 
 
 /**
@@ -96,26 +94,24 @@ add_action( 'init', 'menu_duplicator_process_form' );
  *
  * @since Menu Duplicator 0.1
  */
-function menu_duplicator_screen_check() {
+function menu_duplicator_screen_check()
+{
 
-	// Allow only users with edit_theme_options capabilities.
-	if ( ! current_user_can( 'edit_theme_options' ) ) {
-		return;
-	}
+    // Allow only users with edit_theme_options capabilities.
+    if (! current_user_can('edit_theme_options')) {
+        return;
+    }
 
-	$current_screen = get_current_screen();
-	$menu_count = count( wp_get_nav_menus() );
+    $current_screen = get_current_screen();
+    $menu_count = count(wp_get_nav_menus());
 
-	if ( $current_screen->id === 'nav-menus' and $menu_count ) {
-
-		wp_enqueue_script( 'menu_duplicator_script', MD_PLUGIN_URL . '/scripts/menu-duplicator.js', false, '1.0' );
-		// Output jQuery to create a new tab within the Menus dashboard page.
-		add_action( 'admin_head', 'menu_duplicator_admin_head_js' );
-
-	}
-
+    if ($current_screen->id === 'nav-menus' and $menu_count) {
+        wp_enqueue_script('menu_duplicator_script', MD_PLUGIN_URL . '/scripts/menu-duplicator.js', false, '1.0');
+        // Output jQuery to create a new tab within the Menus dashboard page.
+        add_action('admin_head', 'menu_duplicator_admin_head_js');
+    }
 }
-add_action( 'current_screen', 'menu_duplicator_screen_check' );
+add_action('current_screen', 'menu_duplicator_screen_check');
 
 
 /**
@@ -125,9 +121,10 @@ add_action( 'current_screen', 'menu_duplicator_screen_check' );
  *
  * @since Menu Duplicator 0.2
  */
-function menu_duplicator_admin_head_js() {
-	?>
-	<script type="text/javascript">var MD_TOOLS_PAGE = "<?php echo esc_url( MD_TOOLS_PAGE ); ?>";</script>
+function menu_duplicator_admin_head_js()
+{
+    ?>
+	<script type="text/javascript">var MD_TOOLS_PAGE = "<?php echo esc_url(MD_TOOLS_PAGE); ?>";</script>
 	<?php
 }
 
@@ -139,17 +136,15 @@ function menu_duplicator_admin_head_js() {
  *
  * @since Menu Duplicator 0.1
  */
-function menu_duplicator_settings_page() {
-
-	$nav_menus = wp_get_nav_menus();
-
-?>
+function menu_duplicator_settings_page()
+{
+    $nav_menus = wp_get_nav_menus(); ?>
 <div class="wrap">
 
     <h1>Menu Duplicator</h1>
 
     <div id="menu-duplicator-wrap">
-        <form method="post" action="<?php echo esc_attr( MD_TOOLS_PAGE ); ?>">
+        <form method="post" action="<?php echo esc_attr(MD_TOOLS_PAGE); ?>">
             <table class="form-table">
                 <tbody>
                     <tr>
@@ -159,9 +154,9 @@ function menu_duplicator_settings_page() {
                         <td>
                             <select id="menu-to-duplicate" name="menu-to-duplicate" required>
                                 <option value="">&mdash; Select a Menu &mdash;</option>
-                                <?php foreach ( $nav_menus as $menu ) : ?>
-                                    <option value="<?php echo esc_attr( $menu->term_id ); ?>">
-                                        <?php echo esc_html( wp_html_excerpt( $menu->name, 40, '&hellip;' ) ); ?>
+                                <?php foreach ($nav_menus as $menu) : ?>
+                                    <option value="<?php echo esc_attr($menu->term_id); ?>">
+                                        <?php echo esc_html(wp_html_excerpt($menu->name, 40, '&hellip;')); ?>
                                     </option>
                                 <?php endforeach; ?>
                             </select>
@@ -178,13 +173,14 @@ function menu_duplicator_settings_page() {
                 </tbody>
             </table>
             <input type="hidden" name="type" value="menu-duplicator">
-            <?php submit_button( 'Duplicate', 'primary', 'submit', true ); ?>
-            <?php wp_nonce_field( 'menu-duplicator','menu-duplicator-nonce' ); ?>
+            <?php submit_button('Duplicate', 'primary', 'submit', true); ?>
+            <?php wp_nonce_field('menu-duplicator', 'menu-duplicator-nonce'); ?>
         </form>
     </div>
 
 </div>
-<?php }
+<?php
+}
 
 
 /**
@@ -197,67 +193,65 @@ function menu_duplicator_settings_page() {
  *
  * @since Menu Duplicator 0.1
  */
-function menu_duplicator_settings_update( $menu_to_duplicate, $new_menu_name ) {
+function menu_duplicator_settings_update($menu_to_duplicate, $new_menu_name)
+{
+    if (! isset($menu_to_duplicate)) { // Input var okay.
+        menu_duplicator_admin_message('error', 'Menu to duplicate was not supplied. Please select a menu from the list below.');
+        return;
+    }
 
-	if ( ! isset( $menu_to_duplicate ) ) { // Input var okay.
-		menu_duplicator_admin_message( 'error', 'Menu to duplicate was not supplied. Please select a menu from the list below.' );
-		return;
-	}
+    if (! isset($new_menu_name)) { // Input var okay.
+        menu_duplicator_admin_message('error', 'Name for the new menu was not supplied. Please input a name for the menu below.');
+        return;
+    }
 
-	if ( ! isset( $new_menu_name ) ) { // Input var okay.
-		menu_duplicator_admin_message( 'error', 'Name for the new menu was not supplied. Please input a name for the menu below.' );
-		return;
-	}
+    $existing_menu_id = intval($menu_to_duplicate); // Input var okay.
+    $new_menu_name = sanitize_text_field($new_menu_name); // Input var okay.
+    $new_menu_id = wp_create_nav_menu($new_menu_name);
+    $existing_menu_items = wp_get_nav_menu_items($existing_menu_id);
 
-	$existing_menu_id = intval( $menu_to_duplicate ); // Input var okay.
-	$new_menu_name = sanitize_text_field( $new_menu_name ); // Input var okay.
-	$new_menu_id = wp_create_nav_menu( $new_menu_name );
-	$existing_menu_items = wp_get_nav_menu_items( $existing_menu_id );
+    if (is_wp_error($new_menu_id)) {
+        menu_duplicator_admin_message('error', 'Menu <strong>'.$new_menu_name.'</strong> already exists, please select a different name.');
+        return;
+    }
 
-	if ( is_wp_error( $new_menu_id ) ) {
-		menu_duplicator_admin_message( 'error', 'Menu <strong>'.$new_menu_name.'</strong> already exists, please select a different name.' );
-		return;
-	}
+    // [Loop each existing menu item, to create a new menu item with the new menu id.
+    foreach ($existing_menu_items as $key => $value) {
 
-	// [Loop each existing menu item, to create a new menu item with the new menu id.
-	foreach ( $existing_menu_items as $key => $value ) {
+        // Create new menu item to get the id.
+        $new_menu_item_id = wp_update_nav_menu_item($new_menu_id, 0, null);
 
-		// Create new menu item to get the id.
-		$new_menu_item_id = wp_update_nav_menu_item( $new_menu_id, 0, null );
+        // Store all parent child relationships in an array.
+        $parent_child[ $value->db_id ] = $new_menu_item_id;
 
-		// Store all parent child relationships in an array.
-		$parent_child[ $value->db_id ] = $new_menu_item_id;
+        if (isset($parent_child[ $value->menu_item_parent ])) {
+            $menu_item_parent_id = $parent_child[ $value->menu_item_parent ];
+        } else {
+            $menu_item_parent_id = 0;
+        }
 
-		if ( isset( $parent_child[ $value->menu_item_parent ] ) ) {
-			$menu_item_parent_id = $parent_child[ $value->menu_item_parent ];
-		} else {
-			$menu_item_parent_id = 0;
-		}
+        $args = array(
+            'menu-item-db-id'       => $value->db_id,
+            'menu-item-object-id'   => $value->object_id,
+            'menu-item-object'      => $value->object,
+            'menu-item-parent-id'   => intval($menu_item_parent_id),
+            'menu-item-position'    => $value->menu_order,
+            'menu-item-type'        => $value->type,
+            'menu-item-title'       => $value->title,
+            'menu-item-url'         => $value->url,
+            'menu-item-description' => $value->description,
+            'menu-item-attr-title'  => $value->attr_title,
+            'menu-item-target'      => $value->target,
+            'menu-item-classes'     => implode(' ', $value->classes),
+            'menu-item-xfn'         => $value->xfn,
+            'menu-item-status'      => $value->post_status,
+        );
 
-		$args = array(
-			'menu-item-db-id'       => $value->db_id,
-			'menu-item-object-id'   => $value->object_id,
-			'menu-item-object'      => $value->object,
-			'menu-item-parent-id'   => intval( $menu_item_parent_id ),
-			'menu-item-position'    => $value->menu_order,
-			'menu-item-type'        => $value->type,
-			'menu-item-title'       => $value->title,
-			'menu-item-url'         => $value->url,
-			'menu-item-description' => $value->description,
-			'menu-item-attr-title'  => $value->attr_title,
-			'menu-item-target'      => $value->target,
-			'menu-item-classes'     => implode( ' ', $value->classes ),
-			'menu-item-xfn'         => $value->xfn,
-			'menu-item-status'      => $value->post_status,
-		);
+        // Update the menu nav item with all information.
+        wp_update_nav_menu_item($new_menu_id, $new_menu_item_id, $args);
+    }
 
-		// Update the menu nav item with all information.
-		wp_update_nav_menu_item( $new_menu_id, $new_menu_item_id, $args );
-
-	}
-
-	menu_duplicator_admin_message( 'success', '<strong>' . esc_html( $new_menu_name ) . '</strong> menu has been created. <a href="' . esc_url( admin_url( 'nav-menus.php' ) ) . '?action=edit&menu=' . $new_menu_id . '">Edit Menu</a>' );
-
+    menu_duplicator_admin_message('success', '<strong>' . esc_html($new_menu_name) . '</strong> menu has been created. <a href="' . esc_url(admin_url('nav-menus.php')) . '?action=edit&menu=' . $new_menu_id . '">Edit Menu</a>');
 }
 
 
@@ -271,18 +265,15 @@ function menu_duplicator_settings_update( $menu_to_duplicate, $new_menu_name ) {
  * @param string $status Type of message to dipaly. Default error.
  * @param string $message Message to display.
  */
-function menu_duplicator_admin_message( $status, $message ) {
+function menu_duplicator_admin_message($status, $message)
+{
+    add_action('admin_notices', function () use ($status, $message) {
+        if (! in_array($status, array( 'error', 'warning', 'success', 'info' ), true)) {
+            $class = 'error';
+        } else {
+            $class = $status;
+        }
 
-	add_action( 'admin_notices', function() use ( $status, $message ) {
-
-		if ( ! in_array( $status, array( 'error', 'warning', 'success', 'info' ), true ) ) {
-			$class = 'error';
-		} else {
-			$class = $status;
-		}
-
-		echo '<div class="notice notice-' . esc_attr( $class ) . ' is-dismissible"><p>' . $message . '</p></div>'; // WPCS: xss ok.
-
-	});
-
+        echo '<div class="notice notice-' . esc_attr($class) . ' is-dismissible"><p>' . $message . '</p></div>'; // WPCS: xss ok.
+    });
 }
